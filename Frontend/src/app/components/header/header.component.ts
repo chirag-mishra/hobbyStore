0e608c190bbbff0aa8ca21e88a39fa3049667532
SanatSmntry <sanatsamantray22@gmail.com>
2018-07-13 23:45:04 +0530

    Added dropdown in header

--- Frontend/src/app/components/header/header.component.ts
+++ Frontend/src/app/components/header/header.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
+  categories:any[] = ["Premium Cards","Close Up Magic","Intermediate Tricks","Beginner Tricks","Street Magic","Mentalism","Stage Magic","Accessories","Books"];
+  bestSellers:any[]=["Huppin's","Popjens","Bicycle"]
 }
