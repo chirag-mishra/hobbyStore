9e468936cae996222737aaaa19aed09da0db2915
SanatSmntry <sanatsamantray22@gmail.com>
2018-07-14 18:46:40 +0530

    created objects array for product catalogue

--- Frontend/src/app/components/products/products.component.ts
+++ Frontend/src/app/components/products/products.component.ts
@@ -7,5 +7,50 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
-
+  productsObject = {
+    products: [
+      {
+        "id": 1,
+        "imgUrl": "assets/images/demo.jpg",
+        "title": "Bicycle",
+        "price": 250,
+        "rating": 5
+      },
+      {
+        "id": 2,
+        "imgUrl": "assets/images/demo.jpg",
+        "title": "Bicycle",
+        "price": 250,
+        "rating": 5
+      },
+      {
+        "id": 3,
+        "imgUrl": "assets/images/demo.jpg",
+        "title": "Bicycle",
+        "price": 250,
+        "rating": 5
+      },
+      {
+        "id": 4,
+        "imgUrl": "assets/images/demo.jpg",
+        "title": "Bicycle",
+        "price": 250,
+        "rating": 5
+      },
+      {
+        "id": 5,
+        "imgUrl": "assets/images/demo.jpg",
+        "title": "Bicycle",
+        "price": 250,
+        "rating": 5
+      },
+      {
+        "id": 6,
+        "imgUrl": "assets/images/demo.jpg",
+        "title": "Bicycle",
+        "price": 250,
+        "rating": 5
+      }
+    ]
+  }
 }
