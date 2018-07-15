fe7b98051d0eaa0ccc1bfbc73cdd56b79a5dc0b7
abhilashMohanty99 <abhilashmohanty99@gmail.com>
2018-07-15 14:14:22 +0530

    made array for toolbar content

--- Frontend/src/app/components/toolbar/toolbar.component.ts
+++ Frontend/src/app/components/toolbar/toolbar.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
-
-
+  title: any = ["Cards", "Premium Cards", "Intermmediate Cards", "Tuple Cards", "Beginner Cards", "Jumbo cards"];
+  toolbarItems: any[] = ["Cards", "Premium Cards", "Intermediate Cards", "Tuple Cards", "Beginner Cards", "Jumbo cards"];
 }
