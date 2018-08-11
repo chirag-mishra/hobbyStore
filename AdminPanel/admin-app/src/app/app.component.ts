8b0c2a1708dd57b8f0a04f4665b2c0d57f1a212a
SanatSmntry <sanatsamantray22@gmail.com>
2018-08-11 15:31:41 +0530

    Added login component in admin proj

--- AdminPanel/admin-app/src/app/app.component.ts
+++ AdminPanel/admin-app/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  onActivate(e) {
+    window.scrollTo(0, 0);
+  }
 }
--- AdminPanel/admin-app/src/app/app.module.ts
+++ AdminPanel/admin-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from "@angular/router";
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AdminComponent } from "./component/admin/admin.component";
@@ -9,6 +9,18 @@ import { StringFilterPipe } from './shared/string-filter.pipe';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from "./component/login/login.component";
+
+
+var routes = [{
+  path:"",
+  component:LoginComponent
+},
+{
+  path:"admin",
+  component:AdminComponent
+}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +33,8 @@ import { LoginComponent } from "./component/login/login.component";
     FormsModule,
     NgxPaginationModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
