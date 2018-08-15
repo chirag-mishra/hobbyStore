fee9375454d3fd84ad0de78d7a354953acc7e287
abhilashMohanty99 <abhilashmohanty99@gmail.com>
2018-08-16 01:50:00 +0530

    Revert "changes made to wrapper class localstorage"
    This reverts commit dc457ee6395cf0c749a98dc7c7b0b31eae15f93a.

--- Frontend/src/app/shared/localStorageWrapper.js
+++ Frontend/src/app/shared/localStorageWrapper.js
@@ -1,82 +1,77 @@
-ls_addToCart = (product) => {
+localStorageWrapper = {
+    //product {"_id" : "id", "quantity" : "qty"}
+    addToCart : (product) => {
 
-    if (!localStorage.getItem("cart")) {
-        let cart = [];
-        cart.push(product);
-        localStorage.setItem("cart", [JSON.stringify(cart)]);
-    }
-    else if (!ls_cartContains(product._id)) {
-        let cart = JSON.parse(localStorage.getItem("cart"));
-        cart.push(product);
-        localStorage.setItem("cart", JSON.stringify(cart));
-    }
-    else {
+        if (!localStorage.getItem("cart")) {
+            let cart = [];
+            cart.push(product);
+            localStorage.setItem("cart", [JSON.stringify(cart)]);
+        }
+        else if (!cartContains(product._id)) {
+            let cart = JSON.parse(localStorage.getItem("cart"));
+            cart.push(product);
+            localStorage.setItem("cart", JSON.stringify(cart));
+        }
+        else {
+            let cart = JSON.parse(localStorage.getItem("cart"));
+            for (singleProduct of cart) {
+                if (singleProduct._id == product._id)
+                    singleProduct.quantity += product.quantity;
+            }
+            localStorage.setItem("cart", JSON.stringify(cart));
+        }
+        return "added to cart";
+    },
+
+increaseQuantity : (product) => {
         let cart = JSON.parse(localStorage.getItem("cart"));
         for (singleProduct of cart) {
             if (singleProduct._id == product._id)
-                singleProduct.quantity += product.quantity;
+                singleProduct.quantity++;
         }
         localStorage.setItem("cart", JSON.stringify(cart));
-    }
-    return "added to cart";
-},
+    },
 
-ls_increaseQuantity = (product) => {
-    let cart = JSON.parse(localStorage.getItem("cart"));
-    for (singleProduct of cart) {
-        if (singleProduct._id == product._id)
-            singleProduct.quantity++;
-    }
-    localStorage.setItem("cart", JSON.stringify(cart));
-},
+decreaseQuantity : (productId) => {
+        let cart = JSON.parse(localStorage.getItem("cart"));
+        let toRemove = null;
+        for (singleProduct of cart) {
+            if (singleProduct._id == productId) {
+                singleProduct.quantity--;
+                if (singleProduct.quantity == 0)
+                    toRemove = singleProduct;
+                break;
+            }
+        }
+        if (toRemove)
+            cart.splice(cart.indexOf(toRemove), 1);
+        localStorage.setItem("cart", JSON.stringify(cart));
+    },
 
-ls_decreaseQuantity = (productId) => {
-    let cart = JSON.parse(localStorage.getItem("cart"));
-    let toRemove = null;
-    for (singleProduct of cart) {
-        if (singleProduct._id == productId) {
-            singleProduct.quantity--;
-            if (singleProduct.quantity == 0)
+removeItemFromCart : (productId) => {
+        let cart = JSON.parse(localStorage.getItem("cart"));
+        let toRemove = null;
+        for (singleProduct of cart) {
+            if (singleProduct._id == productId) {
                 toRemove = singleProduct;
-            break;
+                break;
+            }
         }
-    }
-    if (toRemove)
-        cart.splice(cart.indexOf(toRemove), 1);
-    localStorage.setItem("cart", JSON.stringify(cart));
-},
+        if (toRemove)
+            cart.splice(cart.indexOf(toRemove), 1);
+        localStorage.setItem("cart", JSON.stringify(cart));
+    },
 
-ls_removeItemFromCart = (productId) => {
-    let cart = JSON.parse(localStorage.getItem("cart"));
-    let toRemove = null;
-    for (singleProduct of cart) {
-        if (singleProduct._id == productId) {
-            toRemove = singleProduct;
-            break;
+cartContains : (_id) => {
+        let cart = JSON.parse(localStorage.getItem("cart"));
+        for (product of cart) {
+            if (product._id == _id)
+                return true;
         }
-    }
-    if (toRemove)
-        cart.splice(cart.indexOf(toRemove), 1);
-    localStorage.setItem("cart", JSON.stringify(cart));
-},
+        return false;
+    },
 
-ls_cartContains = (_id) => {
-    let cart = JSON.parse(localStorage.getItem("cart"));
-    for (product of cart) {
-        if (product._id == _id)
-            return true;
+test : () => {
+        alert("hii nigga");
     }
-    return false;
-},
-
-ls_test = () => {
-    alert("hii nigga");
-}
-localStorageWrapper = {
-    //product {"_id" = "id", "quantity" = "qty"}
-    addToCart : ls_addToCart,
-    increaseQuantity: ls_increaseQuantity,
-    decreaseQuantity : ls_decreaseQuantity,
-    removeItemFromCart : ls_removeItemFromCart,
-    test : ls_test
 }
