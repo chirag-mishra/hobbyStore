dc457ee6395cf0c749a98dc7c7b0b31eae15f93a
abhilashMohanty99 <abhilashmohanty99@gmail.com>
2018-08-16 01:02:10 +0530

    changes made to wrapper class localstorage

--- Frontend/src/app/components/product-detail/product-detail.component.ts
+++ Frontend/src/app/components/product-detail/product-detail.component.ts
@@ -1,218 +1,202 @@
 import { Component } from '@angular/core';
 import { ProductRating } from "./../../shared/product-rating";
-
-import { Router,ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
+import { Router } from '@angular/router';
 import { CartsharedService } from '../../shared/cartsharedservice/cartshared.service';
 @Component({
     selector: 'app-product-detail',
     templateUrl: './product-detail.component.html',
-    styleUrls:['./product-detail.component.css']
+    styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent {
-    public prodRatinObj:ProductRating;
-    rate:number;
-    onStarClick:boolean;
-    starRating:number[];
-    qtyInput:number;
-    itemImageUrl:string; 
-    reviewObj :any;
-    outOfStock:boolean;
-    addToCartText:string;
-    inStockText:string;
-    isLoggedIn:boolean;
-    addTocartProduct:any={
-        "_id":'',
-        "quantity":''
-    }
-    productsObj : any = {
-        "_id":"5b65f05916fd446a62cae4b4",
-        "title":"BICYCLE BIG BOX PLAYING CARDS",
-        "description":["Big Box cards are a super-sized alternative!","Want to be a hit at your next game night? Be sure to pick up Bicycle ® Big Box playing cards. A fun, super-sized alternative to regular cards."],
-        "specification":["Bicycle Big Box Playing Cards feature the classic rider back design","Big Box playing cards measure 7\" x 4.5\”","Available in red or blue","Perfect for all ages","Great for play, as a novelty item, for decorating, and more"],
-        "rating":4,
-        "totalReview":61,
-        "price":129,
-        "quantity":1,
-        "imgUrls":["assets/images/bicycleCards/BicycleCard1.jpg","assets/images/bicycleCards/BicycleCard2.jpg","assets/images/bicycleCards/BicycleCard1.jpg"]
+    public prodRatinObj: ProductRating;
+    rate: number;
+    onStarClick: boolean;
+    starRating: number[];
+    qtyInput: number;
+    itemImageUrl: string;
+    reviewObj: any;
+    outOfStock: boolean;
+    addToCartText: string;
+    inStockText: string;
+    productsObj: any = {
+        "title": "BICYCLE BIG BOX PLAYING CARDS",
+        "description": ["Big Box cards are a super-sized alternative!", "Want to be a hit at your next game night? Be sure to pick up Bicycle ® Big Box playing cards. A fun, super-sized alternative to regular cards."],
+        "specification": ["Bicycle Big Box Playing Cards feature the classic rider back design", "Big Box playing cards measure 7\" x 4.5\”", "Available in red or blue", "Perfect for all ages", "Great for play, as a novelty item, for decorating, and more"],
+        "rating": 4,
+        "totalReview": 61,
+        "price": 129,
+        "quantity": 2
     }
     commentObj: any = [
         {
-            userName:"Rakesh",
-            rating:'',
-            title:'',
-            decription:''
+            userName: "Rakesh",
+            rating: '',
+            title: '',
+            decription: ''
         }
     ];
-    similarProductObjects : any=
-  [
-    {
-      products:
-      {
-          "_id": "5b65f05916fd446a92cae4b4",
-          "imgUrl": "assets/images/demo.jpg",
-          "category": "Cards",
-          "title": "Bicycle",
-          "price": 250,
-          "discount": 10,
-          "rating": 5,
-          "date": '2018-06-22'
-      }
-    },
-    {
-      products:
-        {
-            "_id": "5b75f05916fd446a62cae4b4",
-          "imgUrl": "assets/images/demo.jpg",
-          "category":"Cards",
-          "title": "Spades",
-          "price": 250,
-          "discount":20,
-          "rating": 4.4,
-          "date" : '2018-06-02'
-        }
-    },
-    {
-      
-      products:
-        {
-            "_id": "5b65f05816fd446a62cae4b4",
-          "imgUrl": "assets/images/demo.jpg",
-          "category":"Cards",
-          "title": "Hearts",
-          "price": 2500,
-          "discount":30,
-          "rating": 3.5,
-          "date" : '2018-05-24'
-        }
-    }, {
-      
-      products:
-        {
-            "_id": "5h65f05916fd446a62cae4b4",
-          "imgUrl": "assets/images/demo.jpg",
-          "category":"Cards",
-          "title": "Diamond",
-          "price": 250,
-          "discount":40,
-          "rating": 2.3,
-          "date" : '2018-06-13'
-        }
-    }, {
-      
-      products:
-        {
-            "_id": "5765f05916fd446a62cae4b4",
-          "imgUrl": "assets/images/demo.jpg",
-          "category":"Cards",
-          "title": "Ace",
-          "price": 250,
-          "discount":50.5,
-          "rating": 1,
-          "date" : '2018-05-22'
-        }
-    }];
-    constructor(private route:ActivatedRoute,private router:Router,private cartdata:CartsharedService){
-        this.onStarClick=false;
+    similarProductObjects: any =
+        [
+            {
+                "id": 1,
+                products:
+                    {
+                        "imgUrl": "assets/images/demo.jpg",
+                        "category": "Cards",
+                        "title": "Bicycle",
+                        "price": 250,
+                        "discount": 10,
+                        "rating": 5,
+                        "date": '2018-06-22'
+                    }
+            },
+            {
+                "id": 2,
+                products:
+                    {
+                        "imgUrl": "assets/images/demo.jpg",
+                        "category": "Cards",
+                        "title": "Spades",
+                        "price": 250,
+                        "discount": 20,
+                        "rating": 4.4,
+                        "date": '2018-06-02'
+                    }
+            },
+            {
+                "id": 3,
+                products:
+                    {
+                        "imgUrl": "assets/images/demo.jpg",
+                        "category": "Cards",
+                        "title": "Hearts",
+                        "price": 2500,
+                        "discount": 30,
+                        "rating": 3.5,
+                        "date": '2018-05-24'
+                    }
+            }, {
+                "id": 4,
+                products:
+                    {
+                        "imgUrl": "assets/images/demo.jpg",
+                        "category": "Cards",
+                        "title": "Diamond",
+                        "price": 250,
+                        "discount": 40,
+                        "rating": 2.3,
+                        "date": '2018-06-13'
+                    }
+            }, {
+                "id": 5,
+                products:
+                    {
+                        "imgUrl": "assets/images/demo.jpg",
+                        "category": "Cards",
+                        "title": "Ace",
+                        "price": 250,
+                        "discount": 50.5,
+                        "rating": 1,
+                        "date": '2018-05-22'
+                    }
+            }];
+    images: string[] = ["assets/images/bicycleCards/BicycleCard1.jpg", "assets/images/bicycleCards/BicycleCard2.jpg", "assets/images/bicycleCards/BicycleCard1.jpg"];
+    constructor(private route: ActivatedRoute, private router: Router, private cartdata: CartsharedService) {
+        this.onStarClick = false;
         this.inStockText = "In Stock";
-        this.qtyInput=1;
+        this.qtyInput = 1;
         this.addToCartText = "Add to Cart";
         if (this.productsObj.quantity == 0) {
-            this.outOfStock =true;
-            this.qtyInput=0;
+            this.outOfStock = true;
+            this.qtyInput = 0;
             this.addToCartText = "Out of Stock"
         }
-        this.itemImageUrl = this.productsObj.imgUrls[0];
-        this.starRating =[0,1,2,3,4];
-        this.rate=0;
-        this.isLoggedIn =false;
+        this.itemImageUrl = 'assets/images/bicycleCards/BicycleCard1.jpg';
+        this.starRating = [0, 1, 2, 3, 4];
+        this.rate = 0;
         this.reviewObj = [{
-            
-            comment:{
-                "_id":1,
-                "userName":"Mark Buffon",
-                "rating":5,
-                "title":"Very Good!",
-                "description":"Lorem ipsum dolor sit amet, amet condimentum montes ac voluptatum. In et amet ut nunc ipsum, viverra nonummy et, scelerisque leo in nunc velit nec, ultricies vel eros sed potenti condimentum, hendrerit elit curabitur maecenas. Tortor arcu vestibulum et maecenas vivamus integer, sapien eu malesuada vitae pede cursus, sed eu magna gravida dolor."
+            "id": 1,
+            comment: {
+                "userName": "Mark Buffon",
+                "rating": 5,
+                "title": "Very Good!",
+                "description": "Lorem ipsum dolor sit amet, amet condimentum montes ac voluptatum. In et amet ut nunc ipsum, viverra nonummy et, scelerisque leo in nunc velit nec, ultricies vel eros sed potenti condimentum, hendrerit elit curabitur maecenas. Tortor arcu vestibulum et maecenas vivamus integer, sapien eu malesuada vitae pede cursus, sed eu magna gravida dolor."
             }
         },
         {
-            
-            comment:{
-                "_id":2,
-                "userName":"Mark Buffon",
-                "rating":4,
-                "title":"Very Good, Awesome!",
-                "description":"Lorem ipsum dolor sit amet, amet condimentum montes ac voluptatum. In et amet ut nunc ipsum, viverra nonummy et, scelerisque leo in nunc velit nec."
+            "id": 2,
+            comment: {
+                "userName": "Mark Buffon",
+                "rating": 4,
+                "title": "Very Good, Awesome!",
+                "description": "Lorem ipsum dolor sit amet, amet condimentum montes ac voluptatum. In et amet ut nunc ipsum, viverra nonummy et, scelerisque leo in nunc velit nec."
             }
         },
         {
-           
-            comment:{
-                "_id":3,
-                "userName":"Anonymous",
-                "rating":3,
-                "title":"Nice product!",
-                "description":"Lorem ipsum dolor sit amet, amet condimentum montes ac voluptatum. In et amet ut nunc ipsum, viverra nonummy et, scelerisque leo in nunc velit nec, ultricies vel eros sed potenti condimentum, hendrerit elit curabitur maecenas. Tortor arcu vestibulum et maecenas vivamus integer, sapien eu malesuada vitae pede cursus, sed eu magna gravida dolor."
+            "id": 3,
+            comment: {
+                "userName": "Anonymous",
+                "rating": 3,
+                "title": "Nice product!",
+                "description": "Lorem ipsum dolor sit amet, amet condimentum montes ac voluptatum. In et amet ut nunc ipsum, viverra nonummy et, scelerisque leo in nunc velit nec, ultricies vel eros sed potenti condimentum, hendrerit elit curabitur maecenas. Tortor arcu vestibulum et maecenas vivamus integer, sapien eu malesuada vitae pede cursus, sed eu magna gravida dolor."
             }
         },
         {
-         
-            comment:{
-                "_id":4,
-                "userName":"Anonymous",
-                "rating":2,
-                "title":"Very Good!",
-                "description":""
+            "id": 4,
+            comment: {
+                "userName": "Anonymous",
+                "rating": 2,
+                "title": "Very Good!",
+                "description": ""
             }
         },
         {
-          
-            comment:{
-                "_id":5,
-                "userName":"Vikram Yadav",
-                "rating":1,
-                "title":"",
-                "description":"Lorem ipsum dolor sit amet, amet condimentum montes ac voluptatum."
+            "id": 5,
+            comment: {
+                "userName": "Vikram Yadav",
+                "rating": 1,
+                "title": "",
+                "description": "Lorem ipsum dolor sit amet, amet condimentum montes ac voluptatum."
             }
         }]
     }
-    increaseQty(){
+    increaseQty() {
         ++this.qtyInput;
-        if(this.qtyInput>parseInt(this.productsObj.quantity))
-        {
-            this.inStockText ="Sorry, quantity is greater than available stock."
-        }       
+        if (this.qtyInput > parseInt(this.productsObj.quantity)) {
+            this.inStockText = "Sorry, quantity is greater than available stock."
+        }
     }
-    decreaseQty(){
-        this.qtyInput=this.qtyInput-1 < 0 ? 0 : --this.qtyInput;
-        if(this.qtyInput<=parseInt(this.productsObj.quantity)){
+    decreaseQty() {
+        this.qtyInput = this.qtyInput - 1 < 0 ? 0 : --this.qtyInput;
+        if (this.qtyInput <= parseInt(this.productsObj.quantity)) {
             this.inStockText = "In Stock";
         }
-        
-    }   
-    imageChange(value:any){
-        this.itemImageUrl=value.path[0].src;
+
     }
-    onMouseOverRating(value:any){
-        if(!this.onStarClick){
-            this.rate=parseInt(value.target.id);
+    imageChange(value: any) {
+        this.itemImageUrl = value.path[0].src;
+    }
+    onMouseOverRating(value: any) {
+        if (!this.onStarClick) {
+            this.rate = parseInt(value.target.id);
         }
     }
-    onMouseClickRating(value:any){
-        this.onStarClick=!this.onStarClick;
-        this.rate=parseInt(value.target.id);
+    onMouseClickRating(value: any) {
+        this.onStarClick = !this.onStarClick;
+        this.rate = parseInt(value.target.id);
     }
-    onMouseOutRating(value:any){
+    onMouseOutRating(value: any) {
         if (!this.onStarClick) {
-            this.rate=0;
-        } 
+            this.rate = 0;
+        }
     }
-    scrollToRatingDiv (){
+    scrollToRatingDiv() {
         document.querySelector('#reviewForm').scrollIntoView({
-          behavior: 'smooth'
+            behavior: 'smooth'
         });
-      };
-    onCommentSubmit(){
+    };
+    onCommentSubmit() {
         // if(this.rate>0){
         //     this.prodRatinObj = new ProductRating();
         //     this.prodRatinObj.comment.rating = this.rate;
@@ -221,27 +205,14 @@ export class ProductDetailComponent {
         //     console.log(this.prodRatinObj);
         // }
     }
-    AddTotalQuantitytoCart()
-    {
+    AddTotalQuantitytoCart(productQuantity: number) {
         let id = this.route.snapshot.params['id'];
-        
-        this.addTocartProduct={
-            "_id":"",
-            "quantity":""
-        }
-        if (this.isLoggedIn) {
-            //call api
-        }
-        else{
-            this.addTocartProduct._id=id;
-            this.addTocartProduct.quantity = this.qtyInput.toString();
-            localStorage.setItem('cart', JSON.stringify(this.addTocartProduct));
-            //this.cartdata.changecartvalue(1);
-        }
-        
+
+        console.log({ "_id": id, "quantity": this.qtyInput });
+        localStorageWrapper.addToCart({ "_id": id, "quantity": this.qtyInput })
+
     }
-    addsimilarproducttocart()
-    {
+    addsimilarproducttocart() {
         this.cartdata.changecartvalue(1);
     }
 }
--- Frontend/src/app/shared/localStorageWrapper.js
+++ Frontend/src/app/shared/localStorageWrapper.js
@@ -1,77 +1,82 @@
-localStorageWrapper = {
-    //product {"_id" : "id", "quantity" : "qty"}
-    addToCart : (product) => {
+ls_addToCart = (product) => {
 
-        if (!localStorage.getItem("cart")) {
-            let cart = [];
-            cart.push(product);
-            localStorage.setItem("cart", [JSON.stringify(cart)]);
-        }
-        else if (!cartContains(product._id)) {
-            let cart = JSON.parse(localStorage.getItem("cart"));
-            cart.push(product);
-            localStorage.setItem("cart", JSON.stringify(cart));
-        }
-        else {
-            let cart = JSON.parse(localStorage.getItem("cart"));
-            for (singleProduct of cart) {
-                if (singleProduct._id == product._id)
-                    singleProduct.quantity += product.quantity;
-            }
-            localStorage.setItem("cart", JSON.stringify(cart));
-        }
-        return "added to cart";
-    },
-
-increaseQuantity : (product) => {
+    if (!localStorage.getItem("cart")) {
+        let cart = [];
+        cart.push(product);
+        localStorage.setItem("cart", [JSON.stringify(cart)]);
+    }
+    else if (!ls_cartContains(product._id)) {
+        let cart = JSON.parse(localStorage.getItem("cart"));
+        cart.push(product);
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    else {
         let cart = JSON.parse(localStorage.getItem("cart"));
         for (singleProduct of cart) {
             if (singleProduct._id == product._id)
-                singleProduct.quantity++;
+                singleProduct.quantity += product.quantity;
         }
         localStorage.setItem("cart", JSON.stringify(cart));
-    },
-
-decreaseQuantity : (productId) => {
-        let cart = JSON.parse(localStorage.getItem("cart"));
-        let toRemove = null;
-        for (singleProduct of cart) {
-            if (singleProduct._id == productId) {
-                singleProduct.quantity--;
-                if (singleProduct.quantity == 0)
-                    toRemove = singleProduct;
-                break;
-            }
-        }
-        if (toRemove)
-            cart.splice(cart.indexOf(toRemove), 1);
-        localStorage.setItem("cart", JSON.stringify(cart));
-    },
-
-removeItemFromCart : (productId) => {
-        let cart = JSON.parse(localStorage.getItem("cart"));
-        let toRemove = null;
-        for (singleProduct of cart) {
-            if (singleProduct._id == productId) {
-                toRemove = singleProduct;
-                break;
-            }
-        }
-        if (toRemove)
-            cart.splice(cart.indexOf(toRemove), 1);
-        localStorage.setItem("cart", JSON.stringify(cart));
-    },
-
-cartContains : (_id) => {
-        let cart = JSON.parse(localStorage.getItem("cart"));
-        for (product of cart) {
-            if (product._id == _id)
-                return true;
-        }
-        return false;
-    },
-
-test : () => {
-        alert("hii nigga");
     }
+    return "added to cart";
+},
+
+ls_increaseQuantity = (product) => {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    for (singleProduct of cart) {
+        if (singleProduct._id == product._id)
+            singleProduct.quantity++;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+},
+
+ls_decreaseQuantity = (productId) => {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    let toRemove = null;
+    for (singleProduct of cart) {
+        if (singleProduct._id == productId) {
+            singleProduct.quantity--;
+            if (singleProduct.quantity == 0)
+                toRemove = singleProduct;
+            break;
+        }
+    }
+    if (toRemove)
+        cart.splice(cart.indexOf(toRemove), 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+},
+
+ls_removeItemFromCart = (productId) => {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    let toRemove = null;
+    for (singleProduct of cart) {
+        if (singleProduct._id == productId) {
+            toRemove = singleProduct;
+            break;
+        }
+    }
+    if (toRemove)
+        cart.splice(cart.indexOf(toRemove), 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+},
+
+ls_cartContains = (_id) => {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    for (product of cart) {
+        if (product._id == _id)
+            return true;
+    }
+    return false;
+},
+
+ls_test = () => {
+    alert("hii nigga");
+}
+localStorageWrapper = {
+    //product {"_id" = "id", "quantity" = "qty"}
+    addToCart : ls_addToCart,
+    increaseQuantity: ls_increaseQuantity,
+    decreaseQuantity : ls_decreaseQuantity,
+    removeItemFromCart : ls_removeItemFromCart,
+    test : ls_test
 }
